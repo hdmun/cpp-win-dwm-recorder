c0adc2f3053cec8df0a0541e80eb48a4f3bc2938
desktop <>
2020-12-14 13:44:20 +0900

    초기화 코드 - `DwmGetDxSharedSurface` 함수 불러와서 공유 표면 핸들 얻기

--- DwmRecorder/DwmRecorder.cpp
+++ DwmRecorder/DwmRecorder.cpp
@@ -6,9 +6,41 @@ extern "C"{
 namespace dwmrecorder
 {
 
+typedef BOOL(WINAPI* PFDwmGetDxSharedSurface)(
+	HANDLE hHandle,
+	HANDLE* phSurface,
+	LUID* pAdapterLuid,
+	ULONG* pFmtWindow,
+	ULONG* pPresentFlags,
+	ULONGLONG* pWin32kUpdateId
+	);
+
+
+PFDwmGetDxSharedSurface DwmGetDxSharedSurface = nullptr;
 DWMRECORDER_API bool __stdcall initialize(HWND hWnd)
 {
 	LOG("%s, HWND: 0x%x", __FUNCTION__, hWnd);
+
+	HMODULE hUser32Dll = LoadLibraryA("user32.dll");
+	if (hUser32Dll == nullptr) {
+		return false;
+	}
+
+	DwmGetDxSharedSurface = (PFDwmGetDxSharedSurface)GetProcAddress(hUser32Dll, "DwmGetDxSharedSurface");
+	if (DwmGetDxSharedSurface == nullptr) {
+		return false;
+	}
+
+	HANDLE hSurface = nullptr;
+	LUID adapterLuid = { 0, };
+	ULONG pFmtWindow = 0;
+	ULONG pPresentFlags = 0;
+	ULONGLONG pWin32kUpdateId = 0;
+	BOOL bSuccess = DwmGetDxSharedSurface(hWnd, &hSurface, &adapterLuid, &pFmtWindow, &pPresentFlags, &pWin32kUpdateId);
+	if (!bSuccess || !hSurface) {
+		return false;
+	}
+
 	return true;
 }
 
